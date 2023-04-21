de6afe6eba6564fc8b220f76e0a58217958d33c5
Hiruna Lalanga <IT21049590@my.sliit.lk>
2023-04-21 22:22:18 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import AddProduct from "./component/AddProduct";
 import ViewProduct from "./component/ViewProduct";
 import UpdateProduct from "./component/UpdateProduct";
 import Home from "./component/Home";
+import BuyerProduct from "./component/BuyerProduct";
+import Main from "./component/Main";
+import Signup from "./component/SignUp";
+import Login from "./component/Login";
+
 function App() {
+  const user = localStorage.getItem("token")
   return (
     <BrowserRouter>
       <Routes>
@@ -13,6 +19,12 @@ function App() {
         <Route path="/ViewProduct" element={<ViewProduct />} />
         <Route path="/UpdateProduct/:id" element={<UpdateProduct />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/BuyerProduct" element={<BuyerProduct />} />
+        {user&&<Route path="/" exact element={<Main />} />}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate replace to ="/login"/>} />
+
       </Routes>
     </BrowserRouter>
   );
